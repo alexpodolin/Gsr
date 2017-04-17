4f35299f02a0a73ed6d2220896ee6d362280f5a9
Aleksandr Podolin <alex.podolin@gmail.com>
2017-04-17 20:29:24 +0300

    comment

---
+++ js/js.js
@@ -0,0 +1,7 @@
+function showRightMenuHide() {
+   document.getElementById('right-menu-hide').style.display = "block";
+}
+
+function hide() {
+    document.getElementById('right-menu-hide').style.display = 'none';
+}
